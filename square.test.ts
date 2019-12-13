27b80cd5bc656d796719480e3907cd5501df6ce1
Mahendra Choudhary <jakepintu@gmail.com>
2019-12-13 10:30:51 +0530

    Add square util

---
+++ square.test.ts
@@ -0,0 +1,5 @@
+const squareTest = require('./square');
+
+test('square 3 equals to 9', () => {
+  expect(squareTest(3)).toBe(9);
+});
--- sum.test.ts
+++ sum.test.ts
@@ -1,5 +1,5 @@
 const sumTest = require('./sum');
 
-test('adds 1 + 2 to equals 3', () => {
+test('adds 1 + 2 equals to 3', () => {
   expect(sumTest(1, 2)).toBe(3);
 });
